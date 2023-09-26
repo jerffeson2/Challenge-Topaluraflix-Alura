dfa781e79d93747323c5a2fd746f339b93eacd1e
Jerffeson Silva <49159546+jerffeson2@users.noreply.github.com>
2023-09-26 15:26:00 -0300

    Add files via upload

---
+++ src/components/Banner/index.js
@@ -0,0 +1,36 @@
+import NomeCategoria from 'components/NomeCategoria';
+import styles from './Banner.module.css'
+
+function Banner ({categoria}) {
+    const CategoriaNome = categoria.filter((categoria) => {
+        return categoria.nome === "Front-End"
+    })
+
+    const nome = CategoriaNome.map((element) => element.nome);
+
+    return (
+        <section className={styles.container}>
+        <div className={styles.texto}>
+            <NomeCategoria nome={nome} tamanhoTexto="42px"/>
+            <h2></h2>
+            <p></p>
+        </div>
+
+        <div>
+        <iframe
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/Z-N5Fr9P-GU?si=hfGRv1DbvuXRS_aN" 
+        title="YouTube video player" 
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowfullscreen
+        />
+        
+        </div>
+        </section>
+
+    )
+}
+
+export default Banner;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AppRoutes from './routes';
+
+import "@fontsource/roboto/300.css"
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>
+);
+
